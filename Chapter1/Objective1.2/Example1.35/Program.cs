8eb938109a3629126c62d7a3523ef6dd706f1197
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-09-09 18:02:18 +0300

    Added examples for locking variables in multithreading scenarios

---
+++ Chapter1/Objective1.2/Example1.35/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1._35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            var up = Task.Run(() =>
+                {
+                    for (int i = 0; i < 1000000; i++)
+                    {
+                        n++;
+                    }
+                });
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                n--;
+            }
+
+            up.Wait();
+            Console.WriteLine(n);
+
+            Console.ReadKey();
+        }
+    }
+}
