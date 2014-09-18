8ae1a62b2c79ac603ddcb906f20f436d3f4256f8
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-09-18 17:57:22 +0300

    Added example for with Boolean expressions

---
+++ Chapter1/Objective1.3/Example1.58_1.59/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1._58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null;
+            int y = x ?? -1;
+
+            Console.WriteLine(y);
+
+            // Example 1.59
+            Console.WriteLine();
+            Console.WriteLine("Example 1.59");
+
+            int? z = null;
+            int y2 = x ??
+                     z ??
+                     -1;
+
+            Console.WriteLine(y2);
+
+            Console.Write("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+}
