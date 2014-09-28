4ac453ec9716ae49b592788b8e57afe2748cf8af
VladimirAmiorkov <v.amiorkov@gmail.com>
2014-09-28 19:58:30 +0300

    Added examples about events

---
+++ Chapter1/Objective1.4/Example1.83/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1._83
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pub p = new Pub();
+            p.OnChange += OnChange;
+            p.Raise();
+
+            Console.Write("Press a key to exit");
+            Console.ReadKey();
+        }
+
+        public static void OnChange()
+        {
+            Console.WriteLine("Event raised");
+        }
+
+        private class Pub
+        {
+            public event Action OnChange = delegate { };
+
+            public void Raise()
+            {
+                OnChange();
+            }
+        }
+    }
+}
---
+++ Chapter1/Objective1.4/Example1.86/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1._86
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateAndRaise();
+
+            Console.Write("Press a key to exit");
+            Console.ReadKey();
+        }
+
+        public class Pub
+        {
+            public event EventHandler OnChange = delegate { };
+
+            public void Raise()
+            {
+                OnChange(this, EventArgs.Empty);
+            }
+        }
+
+        public static void CreateAndRaise()
+        {
+            Pub p = new Pub();
+            p.OnChange += (sender, e) =>
+                {
+                    Console.WriteLine("Subscrober 1 called");
+                };
+            p.OnChange += (sender, e) =>
+                {
+                    { 
+                        throw new Exception(); 
+                    }
+                };
+            p.OnChange += (sender, e) =>
+                {
+                    Console.WriteLine("Subscriber 3 called"); // Is never called
+                };
+
+            p.Raise();
+        }
+    }
+}
