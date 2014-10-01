bea4e9d2d79ae08e69b8f2d8b5463ff6d354c44f
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-10-01 18:14:00 +0300

    Removed unused using and edited Example 1.23

--- Chapter1/Objective1.1/Example1.23_24_25_26/Program.cs
+++ Chapter1/Objective1.1/Example1.23_24_25_26/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Example1._23
 {
@@ -14,35 +11,43 @@ namespace Example1._23
 
             // 1.23
             // In no particular order
-            //var parallelResult = numbers.AsParallel().
-            //    .Where(i => i % 2 == 0)
-            //    .ToArray();
+            var parallelResult = numbers.AsParallel()
+                .Where(i => i % 2 == 0)
+                .ToArray();
+            Console.WriteLine("Printing parallelResult:");
+            foreach (var number in parallelResult)
+            {
+                Console.WriteLine(number);
+            }
 
             // 1.24
             // Order
-            //var parallelResult = numbers.AsParallel().AsOrdered()
-            //    .Where(i => i % 2 == 0)
-            //    .ToArray();
-
-            //foreach (var number in parallelResult)
-            //{
-            //    Console.WriteLine(number);
-            //}
+            var parallelResult2 = numbers.AsParallel().AsOrdered()
+                .Where(i => i % 2 == 0)
+                .ToArray();
+            Console.WriteLine("Printing parallelResult2:");
+            foreach (var number in parallelResult2)
+            {
+                Console.WriteLine(number);
+            }
 
             // 1.25
-            //var parallelResult = numbers.AsParallel().AsOrdered()
-            //    .Where(i => i % 2 == 0)
-            //    .AsSequential();
+            var parallelResult3 = numbers.AsParallel().AsOrdered()
+                .Where(i => i % 2 == 0)
+                .AsSequential();
+            Console.WriteLine("Printing parallelResult3:");
+            foreach (var number in parallelResult3.Take(5))
+            {
+                Console.WriteLine(number);
+            }
 
-            //foreach (var number in parallelResult.Take(5))
-            //{
-            //    Console.WriteLine(number);
-            //}
-
-            // 1.26
-            var parallelResult = numbers.AsParallel()
+            //// 1.26
+            var parallelResult4 = numbers.AsParallel()
                 .Where(i => i % 2 == 0);
-            parallelResult.ForAll(e => Console.WriteLine(e));
+
+            Console.WriteLine("Printing parallelResult4:");
+            parallelResult4.ForAll(e => Console.WriteLine(e));
+
 
             Console.Write("Press a key to exit");
             Console.ReadKey();
