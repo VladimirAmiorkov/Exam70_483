a8091a53f5f75c9d9d0011ea819c334edd1a873c
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-10-01 17:08:41 +0300

    Added example about exception handling and removed unused usings

--- Chapter1/Objective1.5/Example1.88/Program.cs
+++ Chapter1/Objective1.5/Example1.88/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Example1._88
 {
     class Program
---
+++ Chapter1/Objective1.5/Example1.96/OrderProcessingException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Example1._96
+{
+    public class OrderProcessingException : Exception
+    {
+        public OrderProcessingException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
