b1bed3ea1883d20f120eefb8c9336eeac8f49de9
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-10-03 14:29:05 +0300

    Added example about creating custom Exception class

--- Chapter1/Objective1.5/Example1.96/OrderProcessingException.cs
+++ Chapter1/Objective1.5/Example1.96/OrderProcessingException.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Example1._96
 {
-    public class OrderProcessingException : Exception
+    public class OrderProcessingException : Exception, ISerializable
     {
         public OrderProcessingException(string message, Exception innerException)
             : base(message, innerException)
---
+++ Chapter1/Objective1.5/Example1.98/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1._98
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Possible constructors to use with the custom OrderProcessingExcpetion
+            var errorOrderId = 1;
+            throw new OrderProcessingExcpetion(errorOrderId);
+            throw new OrderProcessingExcpetion(errorOrderId, string.Format("Order {0} is incorrect.", errorOrderId));
+            throw new OrderProcessingExcpetion(errorOrderId, string.Format("Order {0} is incorrect.", errorOrderId), new ArgumentNullException());
+        }
+    }
+}
