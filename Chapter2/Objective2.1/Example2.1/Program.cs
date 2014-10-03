aa8352071e0167af309f43768402b3bc253344a1
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-10-03 18:22:42 +0300

    Added chapter 2 solution and examples about enum and struct

---
+++ Chapter2/Objective2.1/Example2.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Days readingDays = Days.Monday | Days.Saturday;
+        }
+    }
+
+    public enum Days
+    { 
+        None = 0x0,
+        Sunday = 0x1,
+        Monday = 0x2,
+        Tuesday = 0x4,
+        Wednesday = 0x8,
+        Thursday = 0x10,
+        Friday = 0x20,
+        Saturday = 0x40
+    }
+}
---
+++ Chapter2/Objective2.1/Example2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point(1, 1);
+        }
+    }
+
+    public struct Point
+    {
+        public int x, y;
+
+        public Point(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+        }
+    }
+}
