37d8fa4584df98081119c9ac9c8be75aa770fbbb
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-10-09 14:32:23 +0300

    Added example about custom class indexers

---
+++ Chapter2/Objective2.1/Example2.12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class ConstructorChaning
+    {
+        private int p;
+
+        public ConstructorChaning() : this(3){ }
+
+        public ConstructorChaning(int p)
+        {
+            this.p = p;
+        }
+    }
+}
