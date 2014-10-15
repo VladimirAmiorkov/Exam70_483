415a9d4acc886bd7e68fdbf1eb50450eae9a2fe0
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-10-15 18:28:06 +0300

    Added example about dynamic keyword by showing integration of WPF into MS Office Excel

---
+++ Chapter2/Objective2.1/Example2.27/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Example2._27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        void OpenConnection(DbConnection connection)
+        {
+            if (connection is SqlConnection)
+            {
+                // run some special code
+            }
+        }
+
+        void LogStream(Stream stream)
+        {
+            MemoryStream memoryStream = stream as MemoryStream;
+            if (memoryStream != null)
+            {
+                // ...
+            }
+        }
+    }
+}
