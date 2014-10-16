d323387d8681001ef67a9b9494c61d5f1bd73da6
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-10-16 18:20:15 +0300

    Added examples

---
+++ Chapter2/Objective2.1/Example2.29/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Example2._29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic obj = new SampleObject();
+            Console.WriteLine(obj.SomeProperty);
+
+            Console.Write("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+
+    public class SampleObject : DynamicObject
+    {
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = binder.Name;
+            return true;
+        }
+    }
+}
