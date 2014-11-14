9c917cbba5063c93ad79a5f8b59e944df16486bb
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-11-14 18:15:39 +0200

    Added example about preprocessors and assembly protection

---
+++ Chapter3/Objective3.4/Example3.41/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example3._41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeMethod();
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+
+        public static void SomeMethod()
+        {
+#if DEBUG
+            Log("Step1");
+#endif
+        }
+
+        private static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
