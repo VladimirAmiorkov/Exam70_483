41e517bff12b308df472f0d60b28d7da22b99789
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-11-17 18:51:36 +0200

    Added example about: - Logging and  tracing your application - Debugging application

--- Chapter3/Objective3.4/Example3.41/Program.cs
+++ Chapter3/Objective3.4/Example3.41/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Example3._41
 {
@@ -8,6 +9,9 @@ namespace Example3._41
         {
             SomeMethod();
 
+            // Add a breakpoint and hover the person field with the mouse to see the DebuggerDisplay values.
+            var person = new Person("John", "Doe");
+
             Console.WriteLine("Press a key to exit");
             Console.ReadKey();
         }
@@ -15,13 +19,36 @@ namespace Example3._41
         public static void SomeMethod()
         {
 #if DEBUG
-            Log("Step1");
+            Log("Log");
 #endif
+            Log2("Log2");
         }
 
         private static void Log(string message)
         {
             Console.WriteLine(message);
+        }
+
+        // More convenient way of wrapping a method
+
+        [Conditional("DEBUG")]
+        private static void Log2(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        [DebuggerDisplay("Name = {FirstName} {LastName}")]
+        public class Person
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public Person(string firstName, string lastName)
+            {
+                this.FirstName = firstName;
+                this.LastName = lastName;
+            }
         }
     }
 }
